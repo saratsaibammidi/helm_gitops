dba72e4814e111bbe3cf4da8af7e6291f3e18235
saratsaibammidi <sai.sarat2@gmail.com>
2025-12-28 16:49:33 -0500

    Fix Helm chart: deployment and service YAML lint-passing

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return "Hello from Helm + GitHub Actions 🚀"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
